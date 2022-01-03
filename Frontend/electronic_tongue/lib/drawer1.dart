c07fa0c702936b34f9f84f7988a5a09bcc9e408f
SalmaAshour24 <74626730+SalmaAshour24@users.noreply.github.com>
2022-01-03 20:40:37 +0200

    drawer

---
+++ Frontend/electronic_tongue/lib/drawer1.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Drawer1 extends StatefulWidget {
+  @override
+  _Drawer1State createState() => _Drawer1State();
+}
+
+class _Drawer1State extends State<Drawer1> {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        // Important: Remove any padding from the ListView.
+        padding: EdgeInsets.zero,
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 30),
+            child: Row(
+              children: [
+                Image.asset(
+                  'assets/profile.jpeg',
+                  scale: 10.2,
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 30, horizontal: 10),
+                  child: Column(
+                    children: [
+                      Text('Username'),
+                      Text('name@yahoo.com'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Divider(
+            height: 10,
+            thickness: 1.2,
+          ),
+          ListTile(
+            leading: Icon(
+              Icons.home,
+              color: Color(0xff00008b),
+            ),
+            title: Text('Home'),
+            onTap: () {
+              Navigator.pushNamed(context, '/mainscreen');
+            },
+          ),
+          ListTile(
+            leading: Icon(Icons.circle, size: 20, color: Color(0xff00008b)),
+            title: Text('Results'),
+            onTap: () {
+              Navigator.pushNamed(context, '/results');
+            },
+          ),
+          Divider(
+            height: 1,
+            thickness: 1.2,
+          ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app_rounded, color: Color(0xff00008b)),
+            title: Text('Sign out'),
+            onTap: () {
+              Navigator.pushNamed(context, '/signout');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
