b596192902d36a33b8212ded6c6042e841ef4bba
bassant20 <76740757+bassant20@users.noreply.github.com>
2022-02-19 22:05:21 +0200

    api

--- Frontend/electronic_tongue/lib/inputs1.dart
+++ Frontend/electronic_tongue/lib/inputs1.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'API.dart';
 import 'drawer1.dart';
 
 class Input1 extends StatefulWidget {
@@ -10,39 +10,19 @@ class Input1 extends StatefulWidget {
 }
 
 class _Input1State extends State<Input1> {
+  String output = 'Milk Classification';
+  dynamic url;
+  var Data;
+  var inp1, inp2, inp3;
   @override
   Widget build(BuildContext context) {
-    return 
-    Stack(
-      children: <Widget>[
-         Column(
-          children: <Widget>[
-             Container(
-              height: 200,
-              color: Color(0xff506D84),
-            ),
-             Container(
-              height: 580,
-              color: Color(0xffEEEEEE),
-            )
-          ],
-        ),
-         Container(
-          alignment: Alignment.topCenter,
-          padding:  EdgeInsets.only(top: 130, right: 20.0, left: 20.0),
-          child:  Container(
-            height: 600.0,
-            width: MediaQuery.of(context).size.width,
-            child:  Card(
-              child: Scaffold(      drawer: Drawer1(),
-                appBar: AppBar(
-                  //   title: Apps(),
-                  backgroundColor: Color(0xff506D84),
-                ),
-              
-             body: 
-               ListView(
-      children: [
+    return Scaffold(
+      drawer: Drawer1(),
+      appBar: AppBar(
+        //   title: Apps(),
+        backgroundColor: Color(0xff506D84),
+      ),
+      body: ListView(children: [
         const Card(
           // shape: RoundedRectangleBorder(
           //   borderRadius: BorderRadius.circular(35),
@@ -59,97 +39,118 @@ class _Input1State extends State<Input1> {
           ),
           elevation: 30,
           color: Color(0xff506D84),
-          margin: EdgeInsets.only(bottom: 15, top:20,left: 20, right: 20),
+          margin: EdgeInsets.only(bottom: 15, top: 20, left: 20, right: 20),
         ),
         Padding(
-          padding: const EdgeInsets.only(top: 100.0),
-          child:
-              Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
-            Container(
-                width: 70,
-                //  height: 20,
-                child: TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Enter input';
-                      }
-                      return null;
-                    },
-                    cursorColor: Color(0XFFFFCCFF),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        labelText: 'First input',
-                        border: OutlineInputBorder(),
-                        errorBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.red, width: 5))))),
-            Container(
-                width: 70,
-                child: TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Enter input';
-                      }
-                      return null;
-                    },
-                    cursorColor: Color(0XFFFFCCFF),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        labelText: 'Second input',
-                        border: OutlineInputBorder(),
-                        errorBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.red, width: 5))))),
-            Container(
-                width: 70,
-                //  height: 20,
-                child: TextFormField(
-                    validator: (value) {
-                      if (value == null || value.isEmpty) {
-                        return 'Enter input';
-                      }
-                      return null;
-                    },
-                    cursorColor: Color(0XFFFFCCFF),
-                    textAlign: TextAlign.center,
-                    decoration: InputDecoration(
-                        labelText: 'third input',
-                        border: OutlineInputBorder(),
-                        errorBorder: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(color: Colors.red, width: 5)))))
-          ]),
+          padding: const EdgeInsets.only(top: 30.0),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                    width: 300,
+                    height: 60,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Enter input';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            inp1 = value;
+                          },
+                          cursorColor: Color(0XFFFFCCFF),
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                              labelText: 'First input',
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.red, width: 5)))),
+                    )),
+                Container(
+                    width: 300,
+                    height: 70,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Enter input';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            inp2 = value;
+                          },
+                          cursorColor: Color(0XFFFFCCFF),
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                              labelText: 'First input',
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.red, width: 5)))),
+                    )),
+                Container(
+                    width: 300,
+                    height: 70,
+                    child: Padding(
+                      padding: const EdgeInsets.only(bottom: 20.0),
+                      child: TextFormField(
+                          validator: (value) {
+                            if (value == null || value.isEmpty) {
+                              return 'Enter input';
+                            }
+                            return null;
+                          },
+                          onChanged: (value) {
+                            inp3 = value;
+                          },
+                          cursorColor: Color(0XFFFFCCFF),
+                          textAlign: TextAlign.center,
+                          decoration: InputDecoration(
+                              labelText: 'First input',
+                              border: OutlineInputBorder(),
+                              errorBorder: OutlineInputBorder(
+                                  borderSide: BorderSide(
+                                      color: Colors.red, width: 5)))),
+                    ))
+              ]),
+        ),
+        Text(
+          output,
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+          textAlign: TextAlign.center,
         ),
         Padding(
-          padding: const EdgeInsets.only(top: 100),
+          padding: const EdgeInsets.only(top: 20, left: 40, right: 40),
           child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, '/results');
+              onPressed: () async {
+                url = 'http://10.0.2.2:5000/milk?input1=' +
+                    inp1 +'&input2=' + inp2 + '&input3=' + inp3;
+                Data = await Getdata(url);
+                setState(() {
+                  output = Data;
+                });
               },
               style: ElevatedButton.styleFrom(
-                
-                  primary: Color(0xff506D84),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(6),
-                  ),
-                 ),
+                primary: Color(0xff506D84),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(6),
+                ),
+              ),
               child: Text(
                 'Show Output',
                 style: TextStyle(
                   color: Color(0xffffffff),
-                                      fontSize: 20,
-
+                  fontSize: 20,
                 ),
               )),
         ),
       ]),
-              ),
-              color: Color(0xffEEEEEE),
-              elevation: 9.0,
-            ),
-          ),
-        )
-      ],
     );
   }
 }
