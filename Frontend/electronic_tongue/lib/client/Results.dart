52e9eece4b701ecf503e98601100f0a3444089a0
MahaDahshan <mahadahshan@gmail.com>
2022-03-09 20:11:56 +0200

    updates

--- Frontend/electronic_tongue/lib/client/Results.dart
+++ Frontend/electronic_tongue/lib/client/Results.dart
@@ -9,28 +9,7 @@ class Results extends StatefulWidget {
 class _ResultsState extends State<Results> {
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        new Column(
-          children: <Widget>[
-            new Container(
-              height: 200,
-              color: Color(0xff506D84),
-            ),
-            new Container(
-              height: 580,
-              color: Color(0xffEEEEEE),
-            )
-          ],
-        ),
-        new Container(
-          alignment: Alignment.topCenter,
-          padding: new EdgeInsets.only(top: 130, right: 20.0, left: 20.0),
-          child: new Container(
-            height: 580.0,
-            width: MediaQuery.of(context).size.width,
-            child: new Card(
-              child:Scaffold(
+    return Scaffold(
         drawer: Drawer1(),
         appBar: AppBar(
           //   title: Apps(),
@@ -63,23 +42,12 @@ class _ResultsState extends State<Results> {
               Column(children: [
                 Padding(padding: EdgeInsets.all(10)),
                 const Text('Liquid Name: ',
-                    style: TextStyle(
-                        fontSize: 35,
-                        color: Colors.black)),
+                    style: TextStyle(fontSize: 35, color: Colors.black)),
                 const Text('Black Tea -> 70% ',
-                    style: TextStyle(
-                        fontSize: 23,
-                        color: Colors.black))
+                    style: TextStyle(fontSize: 23, color: Colors.black))
               ]),
             ])
           ]),
-        )),
-              color: Color(0xffEEEEEE),
-              elevation: 9.0,
-            ),
-          ),
-        )
-      ],
-    );
+        ));
   }
 }
